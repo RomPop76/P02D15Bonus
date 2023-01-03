97d976bc26ef32e05afa0b416f80c85f7b7cd944
RomPop76 <r.popielas@gmail.com>
2023-01-03 22:27:41 +0100

    Commit 5

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ function changeDot() {
     const heightS2 = $('.s2').offset().top; //wysokość sekcji 2
     const heightS3 = $('.s3').offset().top; //wysokość sekcji 3
     const heightS4 = $('.s4').offset().top; //wysokość sekcji 4
+    // console.log(scrollValue.toFixed(), heightS2.toFixed(), heightS3.toFixed(), heightS4.toFixed());
+
+    // $('li').removeClass('active');
+
+    if (scrollValue < heightS2) {
+        $('nav li').not('.dot1').removeClass('active');
+        $('.dot1').addClass('active');
+    }
+    else if (scrollValue < heightS3) {
+        $('nav li').not('.dot2').removeClass('active');
+        $('.dot2').addClass('active');
+    }
+    else if (scrollValue < heightS4) {
+        $('nav li').not('.dot3').removeClass('active');
+        $('.dot3').addClass('active');
+    }
+    else {
+        $('nav li').not('.dot4').removeClass('active');
+        $('.dot4').addClass('active');
+    }
 }
 
 $(window).on("scroll", changeDot);
